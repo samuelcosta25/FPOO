595996aa07107da41f2e9de4a4348f5cef70a940
samuel.costa25 <samu.live@outlook.com>
2023-02-13 21:56:03 -0300

    13/02/2023

--- Arquivos/Java/AloMundo/CalculadoraDescontos/App.java
+++ Arquivos/Java/AloMundo/CalculadoraDescontos/App.java
@@ -2,6 +2,8 @@ package AloMundo.CalculadoraDescontos;
 
 public class App {
     public static void main(String[] args) {
-        
+        CalculadoraDescontos objeto1=new CalculadoraDescontos();
+        objeto1.calcular20();
+        objeto1.calcular30();
     }
 }
--- Arquivos/Java/AloMundo/CalculadoraDescontos/CalculadoraDescontos.java
+++ Arquivos/Java/AloMundo/CalculadoraDescontos/CalculadoraDescontos.java
@@ -1,6 +1,22 @@
 package AloMundo.CalculadoraDescontos;
+import java.util.Scanner;
 
 public class CalculadoraDescontos {
-        
+    Scanner sc=new Scanner(system.in);
+        public void calcular20(){
+            System.out.println("Informe o preço do produto:");
+            double precoProduto=sc.nextdouble();
+            final int desconto20=20;
+            double precoComDesconto= precoProduto-(precoProduto*desconto20/100);
+            System.out.println("O preço com desconto é " +precoComDesconto+ " reais");
+
+        }
+        public void calcular30(){
+            double precoProduto;
+            precoProduto=200;
+            final int desconto30=30;
+            double precoComDesconto= precoProduto-(precoProduto*desconto30/100);
+            System.out.println("O preço com desconto é " +precoComDesconto+ " reais");
+        }
 
 }
