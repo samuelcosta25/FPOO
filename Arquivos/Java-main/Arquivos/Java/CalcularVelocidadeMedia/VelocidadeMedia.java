afddce3c3aa7ff3adc60683a6ff14a88f9d1a69e
samuelcosta25 <123784115+samuelcosta25@users.noreply.github.com>
2023-02-21 22:09:36 -0300

    Add files via upload

---
+++ Arquivos/Java-main/Arquivos/Java/CalcularVelocidadeMedia/VelocidadeMedia.java
@@ -0,0 +1,10 @@
+package CalcularVelocidadeMedia;
+
+public class VelocidadeMedia {
+    public static void main(String[] args) {
+        int distanciaPercorrida = 150;
+        int tempoGasto = 2;
+        double velocidadeMedia = distanciaPercorrida/tempoGasto;
+        System.out.println(velocidadeMedia);
+    }
+}
---
+++ Arquivos/Java-main/Arquivos/Java/Operações/App.java
@@ -0,0 +1,11 @@
+package Operações;
+
+public class App {
+ public static void main(String[] args) {
+    Operadores objeto1 = new Operadores();
+    objeto1.aritmetica();
+    objeto1.atribuicao();
+    objeto1.relacionais();
+    objeto1.Logicos();
+}
+}
