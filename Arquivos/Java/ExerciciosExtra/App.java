ca3368354b14d5829a54034de8bb709a4716f57e
samuel.costa25 <samu.live@outlook.com>
2023-03-06 20:02:23 -0300

    06.03.2023

--- Arquivos/Java/ExerciciosExtra/App.java
+++ Arquivos/Java/ExerciciosExtra/App.java
@@ -1,16 +1,14 @@
-package ExerciciosIfElse;
+package ExerciciosExtra;
 
 import java.util.Scanner;
 
 public class App {
-
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
-
         Exercicios objetoApp = new Exercicios();
-        System.out.println("Digite qual o exercício deseja executar:");
-        double numeroExercicio = scan.nextDouble();
+
+        System.out.println("Digite qual exercício deseja executar:");
+        int numeroExercicio = scan.nextInt();
         if (numeroExercicio == 1) {
             objetoApp.exercicio1();
         } else if (numeroExercicio == 2) {
@@ -20,15 +18,12 @@ public class App {
         } else if (numeroExercicio == 4) {
             objetoApp.exercicio4();
         } else if (numeroExercicio == 5) {
-            objetoApp.exercicio5();
-        } else if (numeroExercicio == 6) {
-            objetoApp.exercicio6();
-        } else if (numeroExercicio == 7) {
-            objetoApp.exercicio7(); 
+            objetoApp.exercicio5(); 
         } else {
             System.out.println("Digite um número válido.");
         }
         scan.close();
-    }
-}
 
+    }
+    
+}
---
+++ Arquivos/Java/ExerciciosExtra/Exercicios.java
@@ -0,0 +1,29 @@
+package ExerciciosExtra;
+
+import java.util.Scanner;
+
+public class Exercicios {
+    Scanner read = new Scanner(System.in);
+    public void exercicio1(){
+        System.out.println("Digite um valor inteiro: ");
+        int valor1 = read.nextInt();
+        boolean par= valor1 % 2 == 0;
+        if (par==true){
+        System.out.println("O número é par.");
+        } else {
+        System.out.println("O número é impar.");
+        }
+    }
+    public void exercicio2(){
+
+    }
+    public void exercicio3(){
+        
+    }
+    public void exercicio4(){
+        
+    }
+    public void exercicio5(){
+        
+    }
+}
